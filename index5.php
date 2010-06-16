dad9dddff7b0c0fe0212495d5d775588980c4f80
Angad Singh <email.angad@gmail.com>
2010-06-17 00:11:05 +0530

    Snapstory Views (of MVC, codeigniter)

---
+++ index5.php
@@ -0,0 +1,100 @@
+<html>
+<title>SnapStory</title>
+
+<head>
+
+<style>
+body
+{ background: url("images/bg.jpg") repeat;
+}
+
+h2.logo1{
+float:left;
+margin:40px 0px 20px 250px;
+letter-spacing:-4px;
+text-shadow:#424242 2px 2px 2px;
+font-size:50px;
+font-family:sans-serif;
+}
+h3.logo2{
+float:left;
+letter-spacing:-5px;
+font-size:40px;
+font-family:sans-serif;
+color:#272727;
+text-shadow:#474747 2px 2px 2px;
+}
+
+
+#nav{
+float:left;
+margin: 20px 0px 0px 400px;
+}
+
+a.nav, a.navs{
+text-shadow:#1F1F1F 0px 1px 2px;
+font-size:25px;
+color:#474747;
+text-decoration:none;
+font-family:sans-serif;
+text-align:center;
+display:block;
+
+}
+
+a.nav:hover, a.navs:hover
+{
+background:url(images/menuhover.png) repeat;
+-moz-border-radius: 3px;
+-webkit-border-radius: 3px;
+}
+
+a.navs{
+background:url(images/menuhover.png) repeat;
+-moz-border-radius: 10px;
+-webkit-border-radius: 10px;
+color:#383838;
+}
+
+#picture{
+height:400px;
+width:800px;
+float:left;
+margin:10px 250px 10px 220px;
+background:url(images/menuhover.png) repeat;
+-moz-border-radius: 10px;
+-webkit-border-radius: 10px;
+}
+
+</style>
+<script>
+</script>
+
+
+</head>
+
+<body>
+
+<div id = "logo">
+	<a href = "#">
+		<!--<img id = "icon" src = "images/logo.jpg"></img>-->
+	</a>
+	<h2 class = "logo1">Snap<h3 class = "logo2">Story</h3></h2>
+
+</div>
+
+<div id = "nav">
+		<a class = "navs" href = "#">home</a>
+		<a class = "nav" href = "#"> showcase </a>
+		<a class = "nav" href = "#"> account </a>
+		<a class = "nav" href = "#"> more </a>
+</div>
+
+<div id = "picture">
+	<img src = "img.png" class = "dp"/>
+	
+</div>
+
+
+</body>
+</html>
